0d49d5f1424535fa569bb3d79d8833293bf03174
elaitch <lawrenhyder@shaw.ca>
2014-11-17 14:47:35 -0800

    add Fib. layout

--- builds/dwm/config.h
+++ builds/dwm/config.h
@@ -46,6 +46,7 @@ static const Layout layouts[] = {
          { "þ",      tile },     /* first entry is default */
          { "ý",      NULL },     /* no layout function means floating behavior */
          { "ô",      monocle },
+         { "ù",      spiral },
 };
 
 /* key definitions */
@@ -92,6 +93,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+        { MODKEY,                       XK_c,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_Tab,    toggleview,     {0} },
